da1322ec02babf360fb3421ced2d2cb651b1ad62
merkie <merkie@post.com>
2025-07-24 16:47:40 -0500

    update dependencies and package configuration; downgrade chalk to 4.1.2 and add pkg settings in package.json

--- src/bin/teacat.ts
+++ src/bin/teacat.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import chalk from "chalk";
+import * as chalk from "chalk";
 import { loadRC } from "../lib/rc";
 
 const program = new Command();
